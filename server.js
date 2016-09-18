0b6089789221a0f6f724cc71c359dbd729ac6d87
lliu05 <jia988727@hotmail.com>
2016-09-17 22:07:37 -0700

    jslint server.js

--- server.js
+++ server.js
@@ -1,66 +1,60 @@
+"use strict";
+
 var express = require("express");
 var path = require("path");
-var validUrl = require('valid-url');
+var validUrl = require("valid-url");
 var app = express();
 var mongo = require("mongodb").MongoClient;
-var main_page = "https://luna-url-short.herokuapp.com/";
-var mongo_url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener';
+var mainPage = "https://luna-url-short.herokuapp.com/";
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener';
 
 app.use(express.static(path.join(__dirname, 'static/html')));
 app.use(express.static(path.join(__dirname, 'static/css')));
 
-//input original url, output json {original_url, short_url}
-app.get("/new/:url*", function(req, res) {
-    var original = req.url.slice(5);
-    console.log(original);
-    if (validUrl.isUri(original)){      
-        var random_num = Math.floor(Math.random() * 10000) + 1;
-        var shortened = main_page + random_num;
-        
-        var result = {
-            "original_url": original,
-            "short_url": shortened
+//input original url, output json {originalUrl, shortUrl}
+app.get("/new/:url*", function (req, res) {
+    var original = req.url.slice(5),
+        randomNum = Math.floor(Math.random() * 10000) + 1,
+        shortened = mainPage + randomNum,
+        result = {
+            "originalUrl": original,
+            "shortUrl": shortened
         };
-        
-        mongo.connect(mongo_url, function(err, db) {
-            if (err) throw err;
+    if (validUrl.isUri(original)) {
+        mongo.connect(mongoUrl, function (err, db) {
+            if (err) { throw err; }
             var collection = db.collection("url");
-            collection.updateOne({short: random_num}, {$set: {original_url: original}}, {upsert: true}, function(err, data) {
-                if (err) throw err;
+            collection.updateOne({short: randomNum}, {$set: {originalUrl: original}}, {upsert: true}, function (err, data) {
+                if (err) { throw err; }
+                if (data) { console.log(data); }
                 db.close();
             });
         });
-        
         res.send(result);
-    } 
-    else {
-        res.send("Invalid input URL! Valid URL form: http://www.example.com")
+    } else {
+        res.send("Invalid input URL! Please enter a valid URL: http://www.example.com");
     }
-    
 });
 
 
 //input shortened url, direct page to original url page
-app.get("/:short", function(req, res) {
+app.get("/:short", function (req, res) {
     var shortened = parseInt(req.params.short, 10);
-    
-    mongo.connect(mongo_url, function(err, db) {
-        if (err) throw err;
+    mongo.connect(mongoUrl, function (err, db) {
+        if (err) { throw err; }
         var url = db.collection("url");
         url.find({
             short: {
                 $eq: shortened
             }
-        }).toArray(function(err, docs) {
-            if (err) throw err;
-            //console.log(docs[0]);
+        }).toArray(function (err, docs) {
+            if (err) { throw err; }
             console.log(docs);
-            if (docs.length != 0) {
+            if (docs.length !== 0) {
                 res.writeHead(302, {
-                    'Location': docs[0]["original_url"]
+                    'Location': docs[0].originalUrl
                 });
-            }
-            else {
+            } else {
                 res.send({"error": "This url is not on the database or database is still pending"});
             }
             res.end();
